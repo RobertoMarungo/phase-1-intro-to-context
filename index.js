f20b72ea9bc2cbe882d3744d9ba38dfb11c1e3c3
Roberto Marungo <100833599+RobertoMarungo@users.noreply.github.com>
2023-04-21 00:02:22 +0000

    Created Function

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function createEmployeeRecord() {
+
+}
+
+function createEmployeeRecords() {
+
+}
+
+function createTimeInEvent() {
+
+}
+
+function createTimeOutEvent() {
+
+}
+
+function hoursWorkedOnDate() {
+
+}
+
+function wagesEarnedOnDate() {
+
+}
+
+function allWagesFor() {
+
+}
+
+function calculatePayroll() {
+  
+}
