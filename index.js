d3066e3113398797b204e54c5d69f620e8863c6d
Roberto Marungo <100833599+RobertoMarungo@users.noreply.github.com>
2023-04-24 04:22:43 +0000

    update

--- index.js
+++ index.js
@@ -15,46 +15,55 @@ function createEmployeeRecords(employeeData) {
 }
 
 function createTimeInEvent(employeeRecord, punchIn) {
-    const [date, hour] = punchIn.split(" ")
+    const [date, hour] = punchIn.split(" ");
     employeeRecord.timeInEvents.push({
-        type : "TimeIn",
-        hour : parseInt(hour, 10),
-        date
-    })
-    return employeeRecord
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    });
+    return employeeRecord;
 }
 
 function createTimeOutEvent(employeeRecord, punchOut) {
-    const [date, hour] = punchOut.split(" ")
+    const [date, hour] = punchOut.split(" ");
     employeeRecord.timeOutEvents.push({
-        type : "TimeOut",
-        hour : parseInt(hour, 10),
-        date
-    })
-    return employeeRecord
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    });
+    return employeeRecord;
 }
 
 function hoursWorkedOnDate(employeeRecord, dateWorked) {
-    const punchIn = employeeRecord.timeInEvents.find(employee => employee.date === dateWorked)
+    const punchIn = employeeRecord.timeInEvents.find(
+        (employee) => employee.date === dateWorked
+    );
 
-    const punchOut = employeeRecord.timeOutEvents.find(employee =>  employee.date === dateWorked)
+    const punchOut = employeeRecord.timeOutEvents.find(
+        (employee) => employee.date === dateWorked
+    );
 
-    return (punchOut.hour - punchIn.hour) / 100
+    return (punchOut.hour - punchIn.hour) / 100;
 }
 
 function wagesEarnedOnDate(employeeRecord, dateWorked) {
-    const wagesOweForDay = hoursWorkedOnDate(employeeRecord, dateWorked) * employeeRecord.payPerHour
-    return parseFloat(wagesOweForDay.toString())
+    const wagesOweForDay =
+        hoursWorkedOnDate(employeeRecord, dateWorked) *
+        employeeRecord.payPerHour;
+    return parseFloat(wagesOweForDay.toString());
 }
 
 function allWagesFor(employeeRecord) {
-    let datesWorked = employeeRecord.timeInEvents.map(employee => employee.date)
+    let datesWorked = employeeRecord.timeInEvents.map(
+        (employee) => employee.date
+    );
 
-    let payable = datesWorked.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate(employee, d)
-    }, 0)
+    let payable = datesWorked.reduce(
+        (memo, d) => memo + wagesEarnedOnDate(employee, d),
+        0
+    );
 
-    return payable
+    return payable;
 }
 
 function calculatePayroll() {}
